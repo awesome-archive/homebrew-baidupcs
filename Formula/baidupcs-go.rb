397bbf1b8ae2f67bc71c0875ddf38a27b430c4ad
iikira <2571583272@qq.com>
2018-08-18 16:11:26 +0800

    3.5.4

---
+++ Formula/baidupcs-go.rb
@@ -0,0 +1,11 @@
+class BaiduPCSGo < Formula
+    desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
+    homepage "https://github.com/iikira/BaiduPCS-Go"
+    url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.4/BaiduPCS-Go-v3.5.4-darwin-osx-amd64.zip"
+    version "3.5.4"
+    sha256 "bfba3816520425236974ab36d5a552fc174793dc15e0d7fa0c8bbdd13a916a91"
+
+    def install
+        bin.install "baidupcs-go"
+    end
+end
