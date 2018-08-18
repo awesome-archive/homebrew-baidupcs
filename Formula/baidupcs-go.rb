071fd04ffe722101341fe313ef625c12b4c7d324
iikira <2571583272@qq.com>
2018-08-18 16:12:46 +0800

    fix

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -1,4 +1,4 @@
-class BaiduPCSGo < Formula
+class BaidupcsGo < Formula
     desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
     homepage "https://github.com/iikira/BaiduPCS-Go"
     url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.4/BaiduPCS-Go-v3.5.4-darwin-osx-amd64.zip"
